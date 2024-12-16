39a4b90577b948fbc6a217959fe142fc747180eb
Tianzean <124270928+Tianzean@users.noreply.github.com>
2024-12-16 16:36:47 +0000

    Update Singleton.java

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -7,15 +7,19 @@ package Singleton;
  * Static variables create class objects
  */
 public class Singleton {
-    // private constructor
-    private Singleton() {
-    }
-
-    // Create an object of this class at the member location
-    private static Singleton instance = new Singleton();
-
-    // Provide static methods to obtain the object
+    // 1.Prevent to create external new objects to destroying the singleton pattern
+    private Singleton() {}
+    // 2.Saving singleton objects by private variables
+    private static volatile Singleton instance = null;
+    // 3.Provides a public method to get a singleton object
     public static Singleton getInstance() {
+        if (instance == null) { // First vertify
+            synchronized (Singleton.class) {
+                if (instance == null) { // Second vertify
+                    instance = new Singleton(); 
+                }
+            }
+        }
         return instance;
     }
 }
